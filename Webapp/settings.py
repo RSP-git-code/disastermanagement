edf284d1dce8433d0fa9080fd221786a27d21077
Riya Sangram Pawar <pawarriya9999@gmail.com>
2025-08-27 03:59:46 +0530

    Update settings.py

--- Webapp/settings.py
+++ Webapp/settings.py
@@ -126,8 +126,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER") 
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD") # Gmail App Password
+EMAIL_HOST_USER ="rspgitcode@gmail.com"
+EMAIL_HOST_PASSWORD ="uscs vbiz yzvj trgj"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # -------------------
 # API Keys
@@ -156,5 +156,6 @@ STATIC_URL = "static/"
 # Default PK type
 # -------------------
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 
 
