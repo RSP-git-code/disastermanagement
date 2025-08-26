7dcac74343c7197effee0b9d6a2a0932b7afcefa
Riya Sangram Pawar <pawarriya9999@gmail.com>
2025-08-26 22:10:32 +0530

    Update settings.py

--- Webapp/Webapp/settings.py
+++ Webapp/Webapp/settings.py
@@ -122,14 +122,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # -------------------
 # Email configuration (for password reset)
 # -------------------
+import os
+from pathlib import Path
+
+# ----------------------------
+# Email Configuration (from Render env vars)
+# ----------------------------
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "rspgitcode@gmail.com"
-EMAIL_HOST_PASSWORD = "uscs vbiz yzvj trgj"  # Gmail App Password
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")        # your Gmail (set in Render)
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD") 
 
 # -------------------
 # Internationalization
@@ -150,3 +154,4 @@ STATIC_URL = "static/"
 # Default PK type
 # -------------------
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
